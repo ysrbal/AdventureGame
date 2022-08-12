598e08ea5bdcec38788e6db9da6a06f426968555
yasar.bal <ysr.bal@gmail.com>
2022-08-12 14:31:56 +0300

    added : SafeHouse class, SafeHouse Constructions, onLocation method.

---
+++ src/SafeHouse.java
@@ -0,0 +1,16 @@
+public class SafeHouse extends Location{
+
+    public SafeHouse(Player player) {
+        super(player, "1", "Safe House");
+    }
+
+    @Override
+    public boolean onLocation() {
+        System.out.println("----- SAFE HOUSE -----");
+        System.out.println("Welcome to the Safe House. There isn't enemy here. You can rest and healthy.");
+        System.out.println("Your health full now.");
+        System.out.println("----------------------");
+
+        return true;
+    }
+}
