e414aa027b03fbdfdff97f6880f611c5db341c46
yasar.bal <ysr.bal@gmail.com>
2022-08-12 11:14:54 +0300

    added some texts for the introduction to the game.

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class Game {
-    private Player player;
+    private Scanner scan = new Scanner(System.in);
 
-
-    public Game(){
-
+    public void start() {
+        System.out.println("Welcome to the Adventure !!");
+        System.out.print("Please enter the character name : ");
+        String playerName = scan.nextLine();
+        Player p1 = new Player(playerName);
+        System.out.println("We wish you success in this challenging adventure "+p1.getName()+"!!");
+        System.out.println("-------------------------------------------");
+        p1.selectChar();
+        System.out.println("-------------------------------------------");
 
     }
 }
