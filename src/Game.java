c50a9a43eeb12640e1a5570ecf7760c23acad435
yasar.bal <ysr.bal@gmail.com>
2022-08-12 14:40:36 +0300

    added : location selection to progress in the game.

--- src/Game.java
+++ src/Game.java
@@ -8,10 +8,15 @@ public class Game {
         System.out.print("Please enter the character name : ");
         String playerName = scan.nextLine();
         Player p1 = new Player(playerName);
-        System.out.println("We wish you success in this challenging adventure "+p1.getName()+"!!");
+        System.out.println("We wish you success in this challenging adventure " + p1.getName() + "!!");
         System.out.println("-------------------------------------------");
         p1.selectChar();
         System.out.println("-------------------------------------------");
+        System.out.println(p1.getName() + ", this island has been taken over by dark forces. \n" +
+                " We need you to see the good old and bright days again.");
+        p1.selectLoc();
+
 
     }
 }
+
