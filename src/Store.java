7ab12b7de1f29eee8a45a5fb2c483640f253aff1
yasar.bal <ysr.bal@gmail.com>
2022-08-12 14:30:45 +0300

    added : Store class, Store Constructions, onLocation and buy objects.

---
+++ src/Store.java
@@ -0,0 +1,17 @@
+public class Store extends Location {
+    public Store(Player player) {
+        super(player, "2", "Store");
+    }
+
+    @Override
+    public boolean onLocation() {
+        System.out.println("----- STORE -----");
+        System.out.println("Welcome to the store. You can buy different types items here.");
+        return true;
+    }
+
+    public void buy() {
+        System.out.println("What do you want to buy? : ");
+        System.out.println("What kind items do you need ? :");
+    }
+}
