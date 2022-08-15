baaff2acad13396f75d667ed5664fb28821f73db
yasar.bal <ysr.bal@gmail.com>
2022-08-15 19:27:54 +0300

    added : printWeapon and printArmor method

--- src/Store.java
+++ src/Store.java
@@ -1,13 +1,10 @@
 public class Store extends Location {
-
     public Store(Player player) {
         super(player, "2", "Store");
     }
 
     @Override
     public boolean onLocation() {
-        System.out.println("----- STORE -----");
-        System.out.println("Welcome to the store. You can buy different types items here.");
         System.out.println("What do you want to buy? : ");
         System.out.println("-----ITEMS-----");
         System.out.println("1- Weapon" + "\n2- Armor" + "\n3-Exit");
@@ -28,8 +25,8 @@ public class Store extends Location {
                 System.out.println("The force may be with you !!");
                 return true;
         }
-
         return true;
+
     }
 
 
@@ -95,4 +92,3 @@ public class Store extends Location {
     }
 
 }
-
