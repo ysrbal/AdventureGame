256692e6ae4de2966ac5816073f015cd3f63a375
yasar.bal <ysr.bal@gmail.com>
2022-08-23 08:45:08 +0300

    added : when it came home, it was added to recharge

--- src/SafeHouse.java
+++ src/SafeHouse.java
@@ -1,13 +1,14 @@
-public class SafeHouse extends Location{
+public class SafeHouse extends Location {
 
     public SafeHouse(Player player) {
-        super(player, "1", "Safe House");
+        super(player, 1, "Safe House");
     }
 
     @Override
     public boolean onLocation() {
         System.out.println("----- SAFE HOUSE -----");
         System.out.println("Welcome to the Safe House. There isn't enemy here. You can rest and healthy.");
+        this.getPlayer().setHealth(this.getPlayer().getPlayerFirstHealth());
         System.out.println("Your health full now.");
         System.out.println("----------------------");
 
