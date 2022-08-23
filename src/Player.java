24899dac4428b239e82e8657638fce660799a5b5
yasar.bal <ysr.bal@gmail.com>
2022-08-23 08:50:55 +0300

    added : block and playerFirstHealt object, getter and setter method. added : battleloc options added : isWin method

--- src/Player.java
+++ src/Player.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Player {
     Scanner scan = new Scanner(System.in);
     private int damage;
+    private int block;
+    private int playerFirstHealth;
     private int health;
     private int coin;
     private String name;
     private String charName;
+    private Inventory inventory;
 
     public Player(String name) {
         this.name = name;
@@ -14,18 +17,24 @@ public class Player {
         this.health = health;
         this.coin = coin;
         this.charName = charName;
+        this.block = block;
+        this.inventory = new Inventory();
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public int getTotalDamage() {
+        return damage + this.getInventory().getWeapon().getItemDamage();
+    }
+
     public int getDamage() {
-        return this.damage;
+        return damage;
     }
 
     public void setDamage(int damage) {
@@ -54,6 +63,30 @@ public class Player {
 
     public void setCharName(String charName) {
         this.charName = charName;
+    }
+
+    public int getBlock() {
+        return block + this.getInventory().getArmor().getItemBlock();
+    }
+
+    public void setBlock(int block) {
+        this.block = block;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public int getPlayerFirstHealth() {
+        return playerFirstHealth;
+    }
+
+    public void setPlayerFirstHealth(int playerFirstHealth) {
+        this.playerFirstHealth = playerFirstHealth;
     }
 
     public void selectChar() {
@@ -87,28 +120,74 @@ public class Player {
         System.out.println("Name : " + this.getCharName() +
                 "\t Damage :  " + this.getDamage() +
                 "\t Health : " + this.getHealth() +
+                "\t Money : " + this.getCoin() +
+                "\t Block : " + this.getBlock());
+
+    }
+
+    public void printPlayerInfo() {
+        System.out.println("##############");
+        System.out.println("Your stats :");
+        System.out.println("Weapon : " + this.getInventory().getWeapon().getItemName() +
+                "\t Damage :  " + this.getTotalDamage() +
+                "\t Armor : " + this.getInventory().getArmor().getItemName() +
+                "\t Block : " + this.getBlock() +
+                "\t Health : " + this.getHealth() +
                 "\t Money : " + this.getCoin());
 
     }
 
     public void selectLoc() {
+        Location location = null;
         while (true) {
-            Location location = null;
+            if(isWin(this)){
+                System.out.println("#####################");
+                System.out.println("Congratulations !!");
+                System.out.println("You finished the game !! See you next adventure.");
+                break;
+            }
+            printPlayerInfo();
+            System.out.println();
             System.out.println("##### -- Locations -- #####");
+            System.out.println();
             System.out.println("ID : 1 - Safe House --> There is no enemy !!" +
-                    "\nID : 2 - Store \t--> You buy something and improve yourself !!");
+                    "\nID : 2 - Store\t-->You buy something and improve yourself !!" +
+                    "\nID : 3 - Cave\t-->There are some strange noises coming from inside." +
+                    "\nID : 4 - Forest\t-->It's a foggy and dark place. It looks dangerous." +
+                    "\nID : 5 - River\t-->The river is cool and lots of fish available. There may be dangerous creatures that are hungry or thirsty." +
+                    "\nID : 6 - Mine\t-->Strange sounds are coming from the depths." +
+                    "\nID : 0 - Exit\t-->You can leave the game!!");
 
             System.out.print("Please choose one : ");
             int selectLoc = scan.nextInt();
             switch (selectLoc) {
+                case 0:
+                    location = null;
+                    break;
                 case 1:
                     location = new SafeHouse(this);
                     break;
                 case 2:
                     location = new Store(this);
                     break;
+                case 3:
+                    location = new Cave(this);
+                    break;
+                case 4:
+                    location = new Forest(this);
+                    break;
+                case 5:
+                    location = new River(this);
+                    break;
+                case 6:
+                    location = new Mine(this);
+                    break;
                 default:
                     location = new SafeHouse(this);
+            }
+            if (location == null) {
+                System.out.println("I have failed you " + this.getName() + "!! I have failed you.");
+                break;
             }
             if (!location.onLocation()) {
                 System.out.println("GAME OVER !!");
@@ -120,8 +199,13 @@ public class Player {
     public void initChar(GameChar gameChar) {
         this.setDamage(gameChar.getDamage());
         this.setHealth(gameChar.getHealth());
+        this.setPlayerFirstHealth(gameChar.getHealth());
         this.setCoin(gameChar.getCoin());
         this.setCharName(gameChar.getCharName());
+    }
+
+    public boolean isWin(Player player) {
+        return getInventory().isFood() && getInventory().isWater() && getInventory().isWood();
     }
 
 }
