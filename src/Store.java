2ef4f41442eac5292fef8fd181affde6a1fc89ee
yasar.bal <ysr.bal@gmail.com>
2022-08-23 08:42:36 +0300

    added : new item buy codes

--- src/Store.java
+++ src/Store.java
@@ -1,34 +1,39 @@
 public class Store extends Location {
     public Store(Player player) {
-        super(player, "2", "Store");
+        super(player, 2, "Store");
     }
 
     @Override
     public boolean onLocation() {
-        System.out.println("What do you want to buy? : ");
-        System.out.println("-----ITEMS-----");
-        System.out.println("1- Weapon" + "\n2- Armor" + "\n3-Exit");
-        System.out.print("Your choose : ");
-        int selectCase = scan.nextInt();
-        while (selectCase < 1 || selectCase > 3) {
-            System.out.println("Incorrect entry. Please try again.");
-            selectCase = scan.nextInt();
-        }
-        switch (selectCase) {
-            case 1:
-                printWeapon();
-                break;
-            case 2:
-                printArmor();
-                break;
-            case 3:
-                System.out.println("The force may be with you !!");
-                return true;
+        boolean showMenu = true;
+        while (showMenu) {
+            System.out.println("What do you want to buy? : ");
+            System.out.println("-----ITEMS-----");
+            System.out.println("1 - Weapon" + "\n2 - Armor" + "\n3 - Exit");
+            System.out.print("Your choose : ");
+            int selectCase = scan.nextInt();
+            while (selectCase < 1 || selectCase > 3) {
+                System.out.println("Incorrect entry. Please try again.");
+                selectCase = scan.nextInt();
+            }
+            switch (selectCase) {
+                case 1:
+                    printWeapon();
+                    buyWeapon();
+                    break;
+                case 2:
+                    printArmor();
+                    buyArmor();
+                    break;
+                case 3:
+                    System.out.println("The force may be with you !!");
+                    showMenu = false;
+                    break;
+
+            }
         }
         return true;
-
     }
-
 
     public void printWeapon() {
         System.out.println("----- WEAPONS -----");
@@ -38,29 +43,35 @@ public class Store extends Location {
                     "\tDamage : " + wp.getItemDamage() +
                     "\tPrice : " + wp.getItemPrice());
         }
+        System.out.println("0 : Exit");
+    }
+
+    public void buyWeapon() {
         System.out.print("Please choose one : ");
         int selectWeapon = scan.nextInt();
-        while (selectWeapon < 1 || selectWeapon > Weapon.weapons().length) {
+        while (selectWeapon < 0 || selectWeapon > Weapon.weapons().length) {
             System.out.println("Incorrect entry. Please try again.");
             selectWeapon = scan.nextInt();
         }
-        Weapon selectedWeapon = Weapon.getWeaponID(selectWeapon);
-        if (selectedWeapon != null) {
-            if (selectedWeapon.getItemPrice() > this.getPlayer().getCoin()) {
-                System.out.println("There is not enough money!");
-            } else {
-                this.getPlayer().setDamage((this.getPlayer().getDamage() + selectedWeapon.getItemDamage()));
-                System.out.println("You have a nice " + selectedWeapon.getItemName());
-                System.out.println("You are stronger now!");
-                System.out.println("Your damage : " + this.getPlayer().getDamage());
-                System.out.println("---------------------");
-                int balance = this.getPlayer().getCoin() - selectedWeapon.getItemPrice();
-                this.getPlayer().setCoin(balance);
-                System.out.println("Your have " + this.getPlayer().getCoin() + " coins left.");
+        if (selectWeapon != 0) {
+            Weapon selectedWeapon = Weapon.getWeaponID(selectWeapon);
+            if (selectedWeapon != null) {
+                if (selectedWeapon.getItemPrice() > this.getPlayer().getCoin()) {
+                    System.out.println("There is not enough money!");
+                } else {
+                    this.getPlayer().getInventory().setWeapon(selectedWeapon);
+                    System.out.println("You have a nice " + selectedWeapon.getItemName());
+                    System.out.println("Now, you've increase +" + selectedWeapon.getItemDamage() + " damage!");
+                    System.out.println("You are stronger now!");
+                    System.out.println("Your damage : " + this.getPlayer().getTotalDamage());
+                    System.out.println("---------------------");
+                    int balance = this.getPlayer().getCoin() - selectedWeapon.getItemPrice();
+                    this.getPlayer().setCoin(balance);
+                    System.out.println("Your have " + this.getPlayer().getCoin() + " coins left.");
 
+                }
             }
         }
-
     }
 
     public void printArmor() {
@@ -71,24 +82,33 @@ public class Store extends Location {
                     "\tBlock : " + ar.getItemBlock() +
                     "\tPrice : " + ar.getItemPrice());
         }
+        System.out.println("0 : Exit");
+    }
+
+    public void buyArmor() {
         System.out.print("Please choose one : ");
         int selectArmor = scan.nextInt();
-        while (selectArmor < 1 || selectArmor > Armor.armors().length) {
+        while (selectArmor < 0 || selectArmor > Armor.armors().length) {
             System.out.println("Incorrect entry. Please try again.");
             selectArmor = scan.nextInt();
         }
-        Armor selectedArmor = Armor.getArmorID(selectArmor);
-        if (selectedArmor != null) {
-            if (selectedArmor.getItemPrice() > this.getPlayer().getCoin()) {
-                System.out.println("There is not enough money!");
-            } else {
-                System.out.println("You have a nice " + selectedArmor.getItemName());
-                int balance = this.getPlayer().getCoin() - selectedArmor.getItemPrice();
-                this.getPlayer().setCoin(balance);
-                System.out.println("You have " + this.getPlayer().getCoin() + " coins left.");
+        if (selectArmor != 0) {
+            Armor selectedArmor = Armor.getArmorID(selectArmor);
+            if (selectedArmor != null) {
+                if (selectedArmor.getItemPrice() > this.getPlayer().getCoin()) {
+                    System.out.println("There is not enough money!");
+                } else {
+                    this.getPlayer().getInventory().setArmor(selectedArmor);
+                    System.out.println("You have a nice " + selectedArmor.getItemName());
+                    System.out.println("Now, you've increase +" + selectedArmor.getItemBlock() + " block!");
+                    System.out.println("You are stronger now!");
+                    System.out.println("Your block : " + this.getPlayer().getBlock());
+                    System.out.println("---------------------");
+                    int balance = this.getPlayer().getCoin() - selectedArmor.getItemPrice();
+                    this.getPlayer().setCoin(balance);
+                    System.out.println("You have " + this.getPlayer().getCoin() + " coins left.");
+                }
             }
         }
-
     }
-
 }
